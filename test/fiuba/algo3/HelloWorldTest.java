a27742451f43dee1ef5050bd4d23af7025f25f7c
fpenovi <avsracing@hotmail.com>
2016-05-21 17:35:13 -0300

    Agrego paquetes fiuba.algo3 en /src y /test

---
+++ test/fiuba/algo3/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package fiuba.algo3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by franch on 5/21/16.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void TestDePruebaHelloWorld() {
+        Assert.assertTrue(true);
+    }
+
+}
