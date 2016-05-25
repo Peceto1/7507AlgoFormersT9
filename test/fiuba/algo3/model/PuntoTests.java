c65e8d859b82b59488fc061bf6e28264d4967aef
fpenovi <avsracing@hotmail.com>
2016-05-25 19:53:52 -0300

    Agrego tests para la clase Punto

---
+++ test/fiuba/algo3/model/PuntoTests.java
@@ -0,0 +1,38 @@
+package fiuba.algo3.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PuntoTests {
+
+    @Test
+    public void crearDosPuntosIgualesAlCompararlosSonElMismoPunto() {
+        Punto punto1 = new Punto(3, 2, 0);
+        Punto punto2 = new Punto(3, 2, 0);
+
+        Assert.assertEquals(punto1, punto2);
+    }
+
+    @Test
+    public void crearDosPuntosDistintosAlCompararlosSonDistintos() {
+        Punto punto1 = new Punto(1, 2, 1);
+        Punto punto2 = new Punto(1, 2, 0);
+
+        Assert.assertNotEquals(punto1, punto2);
+    }
+
+    @Test
+    public void insertarPuntosEnUnHasheableSeHasheanIgualLosPuntosIguales() {
+
+        Set<Punto> setDePuntos = new HashSet<>();
+        Punto p1 = new Punto(1, 2, 0);
+        Punto p2 = new Punto(1, 2, 0);
+        setDePuntos.add(p1);
+
+        Assert.assertTrue(setDePuntos.contains(p2));
+    }
+
+}
