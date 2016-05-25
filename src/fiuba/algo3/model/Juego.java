5544a8a23f3609b324dbd21d948ff69ff07972c5
fpenovi <avsracing@hotmail.com>
2016-05-24 22:20:51 -0300

    Comienzo a escribir logica de Juego

---
+++ src/fiuba/algo3/model/Juego.java
@@ -0,0 +1,33 @@
+package fiuba.algo3.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Juego {
+
+    private List<Jugador> jugadores;
+    private Arena arena;
+    private int turno;
+
+
+    public Juego(String player1, String player2) {
+
+        this.jugadores = new ArrayList();
+        this.jugadores.add(new Jugador(player1));
+        this.jugadores.add(new Jugador(player2));
+        this.arena = new Arena();
+        this.turno = 0;
+    }
+
+
+    public Jugador getJugadorEnTurno() {
+        int cantJugadores = this.jugadores.size();
+        return this.jugadores.get(turno % cantJugadores);
+    }
+
+
+    public void avanzarTurno() {
+        this.turno++;
+    }
+
+}
---
+++ test/fiuba/algo3/model/JuegoTests.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.model;
+
+public class JuegoTests {
+
+    public void pasarTurnoEnElJuegoYPreguntarPorElJugadorEnElTurnoActual() {
+
+    }
+
+}
