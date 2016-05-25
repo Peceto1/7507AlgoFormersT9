371d430f4193a462285497d8329305dc8bbc1016
fpenovi <avsracing@hotmail.com>
2016-05-24 22:25:11 -0300

    Creo Test de funcionalidad para Juego

--- test/fiuba/algo3/model/JuegoTests.java
+++ test/fiuba/algo3/model/JuegoTests.java
@@ -1,8 +1,44 @@
 package fiuba.algo3.model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class JuegoTests {
 
-    public void pasarTurnoEnElJuegoYPreguntarPorElJugadorEnElTurnoActual() {
+    @Test
+    public void crearJuegoNuevoYPreguntarPorJugadorEnTurnoActualDebeSerElPrimerJugador() {
+
+        String jugador1 = "Jorge";
+        String jugador2 = "Ana";
+        Juego juego = new Juego(jugador1, jugador2);
+        Jugador jugadorEnTurno = juego.getJugadorEnTurno();
+
+        Assert.assertTrue(jugadorEnTurno.getNombre().equals(jugador1));
+    }
+
+    @Test
+    public void crearJuegoNuevoAvanzar1TurnoYPreguntarPorJugadorEnTurnoActualDebeSerElSegundoJugador() {
+
+        String jugador1 = "Jorge";
+        String jugador2 = "Ana";
+        Juego juego = new Juego(jugador1, jugador2);
+        juego.avanzarTurno();
+        Jugador jugadorEnTurno = juego.getJugadorEnTurno();
+
+        Assert.assertTrue(jugadorEnTurno.getNombre().equals(jugador2));
+    }
+
+    @Test
+    public void crearJuegoNuevoAvanzar2TurnosYPreguntarPorJugadorEnTurnoActualDebeSerElPrimerJugador() {
+
+        String jugador1 = "Jorge";
+        String jugador2 = "Ana";
+        Juego juego = new Juego(jugador1, jugador2);
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        Jugador jugadorEnTurno = juego.getJugadorEnTurno();
+
+        Assert.assertTrue(jugadorEnTurno.getNombre().equals(jugador1));
 
     }
 
