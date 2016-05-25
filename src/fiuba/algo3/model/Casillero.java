2cb28434d59d64f81509ce8658ca1673dc478421
fpenovi <avsracing@hotmail.com>
2016-05-25 19:01:33 -0300

    Inicializo Punto y Casillero

---
+++ src/fiuba/algo3/model/Casillero.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.model;
+
+public class Casillero {
+
+    // private HabitableEnArena contenido; ---> Algoformer
+    // private Terreno terreno; ---> Objeto Aire/Tierra
+    // private Trampa trampa; ---> Espinas, Nebulosa, etc
+
+    public Casillero() {
+        // Pasarle un tipo de Terreno por parametro por ejemplo Aire o Tierra
+    }
+
+}
---
+++ src/fiuba/algo3/model/Punto.java
@@ -0,0 +1,38 @@
+package fiuba.algo3.model;
+
+public class Punto {
+
+    private int x;
+    private int y;
+    private int nivel;
+
+
+    public Punto(int x, int y, int nivel) {
+        this.x = x;
+        this.y = y;
+        this.nivel = nivel;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Punto punto = (Punto) o;
+
+        if (x != punto.x) return false;
+        if (y != punto.y) return false;
+        return nivel == punto.nivel;
+
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        result = 31 * result + nivel;
+        return result;
+    }
+}
