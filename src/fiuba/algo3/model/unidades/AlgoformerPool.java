6326b8ddcf77640c8fb8aea796d0956656b2627c
fpenovi <avsracing@hotmail.com>
2016-05-28 19:15:31 -0300

    Modifico setear_diccionario a inicializar y la hago publica para poder usarla para setUp en los tests

--- src/fiuba/algo3/model/unidades/AlgoformerPool.java
+++ src/fiuba/algo3/model/unidades/AlgoformerPool.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class AlgoformerPool {
 
-	private static Map<String, Algoformer> mapa = new HashMap<String, Algoformer>();
+	private static Map<String, Algoformer> mapa = new HashMap<>();
 	private static AlgoformerPool instancia = new AlgoformerPool();
 	
 	private String OPTIMUS = "Optimus Prime";
@@ -14,17 +14,14 @@ public class AlgoformerPool {
 	private String MEGATRON = "Megatron";
 	private String FRENZY = "Frenzy";
 	private String BONECRUSHER = "Bonecrusher";
-
-	
-	
 	
 	
 	private AlgoformerPool(){
-		setear_diccionario();
+		this.inicializar();
 	}
 	
-	private void setear_diccionario(){
-		
+	public void inicializar(){
+
 		mapa.put(OPTIMUS, new Autobot(OPTIMUS, 500, OptimusPrimeAlterno.getInstance()));
 		mapa.put(BUMBLEBEE,new Autobot(BUMBLEBEE,350,BumblebeeAlterno.getInstance()));
 		mapa.put(RATCHET, new Autobot(RATCHET, 150, RatchetAlterno.getInstance()));
@@ -33,32 +30,40 @@ public class AlgoformerPool {
 		mapa.put(BONECRUSHER, new Decepticon(BONECRUSHER,200,BoneCrusherAlterno.getInstance()));
 		
 	}
-	
+
+
 	static AlgoformerPool getInstance(){
 		return instancia;
 	}
+
 	
 	public Algoformer obtenerOptimus(){
 		return mapa.get(OPTIMUS);	
 	}
+
+
 	public Algoformer obtenerBumblebee(){
 		return mapa.get(BUMBLEBEE);	
 	}
+
+
 	public Algoformer obtenerRatchet(){
 		return mapa.get(RATCHET);
 	}
+
 		
 	public Algoformer obtenerMegatron(){
 		return mapa.get(MEGATRON);	
 	}
+
+
 	public Algoformer obtenerFrenzy(){
 		return mapa.get(FRENZY);	
 	}
+
+
 	public Algoformer obtenerBonecrusher(){
 		return mapa.get(BONECRUSHER);	
 	}
-	
-	
-	
 	
 }
