3bb11337779ae66dbb757bfa35b426430e5eedb5
SDorgan <santidorgan@gmail.com>
2016-06-04 21:50:10 -0300

    Agrego @Override a las funciones correspondientes

--- src/fiuba/algo3/model/espacio/PuntoAire.java
+++ src/fiuba/algo3/model/espacio/PuntoAire.java
@@ -3,7 +3,6 @@ package fiuba.algo3.model.espacio;
 public class PuntoAire extends Punto{
 
 	public PuntoAire(int x, int y) {
-		// TODO Auto-generated constructor stub
 		super(x, y, 1);
 	}
 	
--- src/fiuba/algo3/model/espacio/PuntoTierra.java
+++ src/fiuba/algo3/model/espacio/PuntoTierra.java
@@ -3,7 +3,6 @@ package fiuba.algo3.model.espacio;
 public class PuntoTierra extends Punto{
 
 	public PuntoTierra(int x, int y) {
-		// TODO Auto-generated constructor stub
 		super(x, y, 0);
 	}
 	
