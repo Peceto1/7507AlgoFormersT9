54744b13dab3e0e308517574fbe37ac2a593dca1
SDorgan <santidorgan@gmail.com>
2016-06-20 19:08:31 -0300

    Rehago la logica de terreno y bonus aleatorio

--- src/fiuba/algo3/model/arena/CreadorDeBonus.java
+++
@@ -1,7 +0,0 @@
-package fiuba.algo3.model.arena;
-
-public interface CreadorDeBonus {
-	
-	Bonus crearBonus();
-
-}
--- src/fiuba/algo3/model/arena/CreadorDeTerreno.java
+++
@@ -1,6 +0,0 @@
-package fiuba.algo3.model.arena;
-
-public interface CreadorDeTerreno {
-	
-	TerrenoAplicable crearTerreno();
-}
