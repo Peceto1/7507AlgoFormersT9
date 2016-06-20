b9a23e7b65c85bcbee6c105466366875deedb2a7
fpenovi <avsracing@hotmail.com>
2016-06-19 22:03:19 -0300

    Corrijo el llamado a setTerrenosAleatorio de Juego

--- src/fiuba/algo3/model/juego/Juego.java
+++ src/fiuba/algo3/model/juego/Juego.java
@@ -24,7 +24,6 @@ public class Juego {
         this.poolSingleton = AlgoformerPool.getInstance();
         this.arenaSingleton = Arena.getInstance();
         this.arenaSingleton.inicializar();
-        this.arenaSingleton.setTerrenoAleatorio();
         this.poolSingleton.inicializar();
         this.ubicarAlgoformers();
         this.inicializarEquipos();
