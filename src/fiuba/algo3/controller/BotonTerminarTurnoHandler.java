96af27cae7a74a4241df1ba2c5e623aaa9b79f2e
SDorgan <santidorgan@gmail.com>
2016-06-21 18:43:23 -0300

    sigo con logica de Boton Terminar Turno

--- src/fiuba/algo3/controller/BotonTerminarTurnoHandler.java
+++ src/fiuba/algo3/controller/BotonTerminarTurnoHandler.java
@@ -11,20 +11,26 @@ public class BotonTerminarTurnoHandler implements EventHandler<ActionEvent> {
     private Label infoJugador;
     private Label infoEquipo;
     private Label infoTurno;
+    private Juego modelJuego;
 
     public BotonTerminarTurnoHandler(PanelLateral panelLateral, Juego juego) {
         this.infoJugador = (Label) panelLateral.lookup("#infoJugador");
         this.infoEquipo = (Label) panelLateral.lookup("#infoEquipo");
         this.infoTurno = (Label) panelLateral.lookup("#infoTurno");
+        this.modelJuego = juego;
     }
 
 
     @Override
     public void handle(ActionEvent actionEvent) {
+    	modelJuego.finalizarTurno();
+    	String nombreJug = modelJuego.getJugadorEnTurno().getNombre();
+        String equipoJug = modelJuego.getJugadorEnTurno().getEquipo();
+        String numeroTurno = Integer.toString(modelJuego.getTurno());
 
-
-
-
+        infoJugador.setText("Jugador: " + nombreJug);
+        infoTurno.setText("Turno: " + numeroTurno);
+        infoEquipo.setText("Equipo: " + equipoJug);
 
 
     }
