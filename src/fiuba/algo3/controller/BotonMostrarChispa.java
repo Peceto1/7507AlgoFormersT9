728a34aaf93bfab154fbee4908752dbb048290bb
SDorgan <santidorgan@gmail.com>
2016-06-29 18:01:53 -0300

    Mejoro el boton buscar chispa para que aparezca un indicador que apunta a la flecha

--- src/fiuba/algo3/controller/BotonMostrarChispa.java
+++ src/fiuba/algo3/controller/BotonMostrarChispa.java
@@ -1,18 +1,24 @@
 package fiuba.algo3.controller;
 
+
 import fiuba.algo3.model.espacio.Punto;
 import fiuba.algo3.model.juego.Juego;
 import fiuba.algo3.view.ContenedorJuego;
+import fiuba.algo3.view.vistas.VistaPosicionChispa;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.util.Duration;
 
 public class BotonMostrarChispa implements EventHandler<ActionEvent> {
 	private ContenedorJuego contenedor;
 	private Juego juego;
+	private VistaPosicionChispa vistaFlecha;
 	
 	public BotonMostrarChispa(ContenedorJuego contenedor){
-	this.contenedor = contenedor;
-	this.juego = contenedor.getJuego();
+		this.vistaFlecha = contenedor.getVistaPosicionChispa();
+		this.contenedor = contenedor;
+		this.juego = contenedor.getJuego();
 	}
 	
 	
@@ -21,6 +27,10 @@ public class BotonMostrarChispa implements EventHandler<ActionEvent> {
 		Punto posicionChispa = juego.getPosicionChispa();
 		contenedor.seleccionarCasillero(posicionChispa.getX(), posicionChispa.getY());
 		contenedor.panearCamara(posicionChispa);
+		vistaFlecha.dibujar();
+		PauseTransition delay = new PauseTransition(Duration.seconds(2));
+		delay.setOnFinished( event -> vistaFlecha.limpiar() );
+		delay.play();
 	}
 
 }
